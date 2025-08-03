c41d31dbf7e7f33aa7097c7a7632e0a21b8605b6
Syed_Hassan <syed_hassan@mvisionit.com>
2025-08-03 07:14:16 +0500

    UI Updates

--- src/app/pages/home/sections/portfolio-section/portfolio-section.component.ts
+++ src/app/pages/home/sections/portfolio-section/portfolio-section.component.ts
@@ -8,15 +8,46 @@ import { Component } from '@angular/core';
 export class PortfolioSectionComponent {
 
   portfolioItems = [
-    { image: 'portfolio-1', filter: 'web', title: 'Go2Sell - E-Commerce Platform' },
-    { image: 'portfolio-2', filter: 'web', title: 'Go2Sell - E-Commerce Platform' },
-    { image: 'portfolio-3', filter: 'web', title: 'Go2Sell - E-Commerce Platform' },
-    { image: 'portfolio-4', filter: 'web', title: 'Go2Sell - E-Commerce Platform' },
-    { image: 'portfolio-5', filter: 'web', title: 'Go2Sell - E-Commerce Platform' },
-    { image: 'portfolio-6', filter: 'app', title: 'Go2Sell - E-Commerce Platform' },
-    { image: 'portfolio-7', filter: 'app', title: 'Go2Sell - E-Commerce Platform' },
-    { image: 'portfolio-8', filter: 'app', title: 'Go2Sell - E-Commerce Platform' },
-    { image: 'portfolio-9', filter: 'app', title: 'Go2Sell - E-Commerce Platform' }
+    // { image: 'portfolio-1', filter: 'web', title: 'Go2Sell - E-Commerce Platform' },
+    // { image: 'portfolio-2', filter: 'web', title: 'Go2Sell - E-Commerce Platform' },
+
+    // UI Hero Shots
+    { image: 'accounting', filter: 'hero', title: 'Invoisku – Smart Banking & Invoicing App for Businesses' },
+    { image: 'engager', filter: 'hero', title: 'Engager – Gamified Onboarding App' },
+    { image: 'finance', filter: 'hero', title: 'UI Finance – All-in-One Financial Support Platform' },
+    { image: 'healthcare', filter: 'hero', title: 'Healers – Healthcare Companion App' },
+    { image: 'llm', filter: 'hero', title: 'Rag Metrices – Evaluation Framework for LLM Applications' },
+    { image: 'pdf', filter: 'hero', title: 'AI Room – Smarter PDF Editing App Powered by AI' },
+
+    { image: 'pepsi', filter: 'hero', title: 'Pepsi – Modern Beverage Brand Concept' },
+    { image: 'mcLaren', filter: 'hero', title: 'McLaren 675LT - Web Concept' },
+    { image: 'discovery', filter: 'hero', title: 'Discovery Channel - Visual Exploration Concept' },
+    { image: 'vr-headset', filter: 'hero', title: 'VR Headset – Immersive Reality UI Concept' },
+    { image: 'apple', filter: 'hero', title: 'Future of Technology - Smart Desktop App' },
+    { image: 'nostalgic-cars', filter: 'hero', title: 'World on Wheels - Car Selling Web App' },
+
+
+    // Landing Lab
+    { image: 'google-store', filter: 'web', title: 'Google - Revamping Store Experience' },
+    { image: 'manateq', filter: 'web', title: 'Manateq - Digital Transformation Website Revamp' },
+    { image: 'grownest', filter: 'web', title: 'GrowNest - Gardening Landing Concept' },
+    { image: 'go2sell', filter: 'web', title: 'Go2Sell - E-Commerce Website Design' },
+    { image: 'mv-website', filter: 'web', title: 'Microvision It Solutions - Enterprise Web Concept' },
+
+    // UX Dashboards
+    { image: 'medicore', filter: 'dashboard', title: 'MediCore – Healthcare Management System' },
+    { image: 'mbrsg', filter: 'dashboard', title: 'MBRSG - School Management System' },
+
+    // App Interfaces
+    { image: 'logify', filter: 'app', title: 'Logify – Smart Daily Tracker App' },
+    { image: 'lets-chat', filter: 'app', title: 'Lets Chat – Instant Messaging App' },
+    { image: 'sony-ps5', filter: 'app', title: 'PS5 Discover – Explore App' },
+
+    // Freestyle
+    { image: 'bank-cards', filter: 'other', title: 'Standard Chartered – Banking Cards Revamp' },
+    { image: 'mechanic', filter: 'other', title: 'Auto Service - Street banner' },
+
+
   ];
 
 }
